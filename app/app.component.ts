682219ff3bb8bb01243f1ac18e5428cc9c4ae678
martin byrne <marb61a@gmail.com>
2016-08-11 10:33:51 +0000

    update app component with starting position and zoom level

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,11 @@
 import {Component} from '@angular/core';
-import {ANGULAR2_GOOGLE_MAPS_DIRECTIVES} from 'angular2-google-maps/core';
+import {
+    ANGULAR2_GOOGLE_MAPS_DIRECTIVES,
+    MapsAPILoader,
+    NoOpMapsAPILoader,
+    MouseEvent
+} from 'angular2-google-maps/core';
+
 
 @Component({
     moduleId: module.id,
@@ -14,6 +20,30 @@ import {ANGULAR2_GOOGLE_MAPS_DIRECTIVES} from 'angular2-google-maps/core';
 })
 
 export class AppComponent { 
-    lat: number = 42.858217;
-    lng: number = -70.929990;
+        // Zoom Level
+        zoom: number = 10;
+        
+        // Starting Position
+        lat: number = 42.858217;
+        lng: number = -70.929990;
+        markers: marker[] = [
+        {
+            name: 'Company One',
+            lat: 42.825588,
+            lng: -71.018029,
+            draggable: true
+        },
+        {
+            name: 'Company Two',
+            lat: 42.868164,
+            lng: -70.889071,
+            draggable: true
+        },
+        {
+            name: 'Company Three',
+            lat: 42.858279,
+            lng: -70.930498,
+            draggable: false
+        }
+    ];
 }
