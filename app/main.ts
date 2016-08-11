59c264c50cf6bb47aac852f081e05ed1eeb57959
martin byrne <marb61a@gmail.com>
2016-08-11 10:39:11 +0000

    main.ts and main.js

---
+++ app/main.ts
@@ -0,0 +1,5 @@
+import {bootstrap}    from '@angular/platform-browser-dynamic';
+import {AppComponent} from './app.component';
+import {ANGULAR2_GOOGLE_MAPS_PROVIDERS} from 'angular2-google-maps/core';
+
+bootstrap(AppComponent, [ANGULAR2_GOOGLE_MAPS_PROVIDERS]);
