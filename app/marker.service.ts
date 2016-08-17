254c443216166667d894d5b987290c85a0d97ef9
martin byrne <marb61a@gmail.com>
2016-08-17 20:33:44 +0000

    updated marker service ts

--- app/marker.service.ts
+++ app/marker.service.ts
@@ -23,4 +23,32 @@ export class MarkerService extends Init {
         // Set markers again
         localStorage.setItem('markers', JSON.stringify(markers));
     }
+    
+    updateMarker(marker : any, newLat : number, newLng : number){
+        var markers = JSON.parse(localStorage.getItem(markers));
+        
+        for(var i = 0; i < markers.length; i++){
+            if(marker.lat == markers[i].lat && marker.lng == markers[i].lng){
+                markers[i].lat = newLat;
+                markers[i].lng = newLng;
+            }
+        }
+        
+        // Set new markerset
+        localStorage.setItem('markers', JSON.stringify(markers));    
+    }
+    
+    removeMarker(marker : any){
+        // Fetch markers
+        var markers = JSON.parse(localStorage.getItem(markers));
+        
+        for(var i = 0; i < markers.length; i++){
+            if(marker.lat == markers[i].lat && marker.lng == markers[i].lng){
+                markers.splice(i, 1);    
+            }
+        }
+        
+        // Set new markerset
+        localStorage.setItem('markers', JSON.stringify(markers));
+    }
 }
