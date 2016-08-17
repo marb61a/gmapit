50960e4a1062e8df7ea3efb20148d6a92ed67f51
martin byrne <marb61a@gmail.com>
2016-08-17 08:11:46 +0000

    updated app component ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {FormBuilder, ControlGroup, Validators, Control} from '@angular/common';
+import {MarkerService} from './marker.service';
 import {
     ANGULAR2_GOOGLE_MAPS_DIRECTIVES,
     MapsAPILoader,
@@ -12,6 +14,7 @@ import {
     selector: 'my-app',
     templateUrl: 'app.component.html',
     directives: [ANGULAR2_GOOGLE_MAPS_DIRECTIVES],
+    providers: [MarkerService],
     styles: [`
         .sebm-google-map-container{
             height:300px;
@@ -26,29 +29,20 @@ export class AppComponent {
         // Starting Position
         lat: number = 42.858217;
         lng: number = -70.929990;
-        markers: marker[] = [
-        {
-            name: 'Company One',
-            lat: 42.825588,
-            lng: -71.018029,
-            draggable: true
-        },
-        {
-            name: 'Company Two',
-            lat: 42.868164,
-            lng: -70.889071,
-            draggable: true
-        },
-        {
-            name: 'Company Three',
-            lat: 42.858279,
-            lng: -70.930498,
-            draggable: false
-        }
-    ];
+        markerForm: ControlGroup;
+        markers: marker[];
     
-    constructor(){
+    constructor(private _markerService : MarkerService){
+        let fb = new FormBuilder();
         
+        this.markerForm = fb.group({
+            name: ['',Validators.required],
+            lat: ['',Validators.required],
+            lng: ['',Validators.required],
+            draggable: [false]     
+        });
+        
+        this.markers = this._markerService.getMarkers();
     }
     
     clickedMarker(marker : marker, index : number){
@@ -67,7 +61,19 @@ export class AppComponent {
     }
     
     markerDragEnd(marker: any, $event: MouseEvent){
+        console.log('dragEnd', marker, $event);
         
+        var updMarker = {
+            name: marker.name,
+            lat: parseFloat(marker.lat),
+            lng: parseFloat(marker.lng),
+            draggable: false
+        }
+        
+        var newLat = $event.coords.lat;
+        var newLng = $event.coords.lng;
+        
+        this._markerService.updateMarker(updMarker, newLat, newLng);
     }
 }
 
